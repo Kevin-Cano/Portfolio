7cc0320940f842904d8365e031574e7fc0bf897b
cakevin <kevin.cano@ynov.com>
2025-11-12 09:51:48 +0100

    minor fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,9 +12,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        // Charger les projets depuis le fichier YAML
         $projectsFile = $this->getParameter('kernel.project_dir') . '/config/projects.yaml';
-        
+
         $projects = [];
         if (file_exists($projectsFile)) {
             $data = Yaml::parseFile($projectsFile);
@@ -29,14 +28,13 @@ class HomeController extends AbstractController
     #[Route('/project/{slug}', name: 'project_detail')]
     public function projectDetail(string $slug): Response
     {
-        // Charger les projets
         $projectsFile = $this->getParameter('kernel.project_dir') . '/config/projects.yaml';
-        
+
         $project = null;
         if (file_exists($projectsFile)) {
             $data = Yaml::parseFile($projectsFile);
             $projects = $data['projects'] ?? [];
-            
+
             foreach ($projects as $p) {
                 if (($p['slug'] ?? '') === $slug) {
                     $project = $p;
